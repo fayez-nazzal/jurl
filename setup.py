a704234cb3280d3c13e52c262e5e42f493f3f7d2
mika <soulldev@gmail.com>
2019-11-30 13:38:05 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jurl',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='mika',
+    author_email='',
+    description=''
+)
